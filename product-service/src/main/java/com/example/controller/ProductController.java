01f5ba2d1c3de5a7740c899ee60c0a937809ba7d
vijay <vijay2340025@gmail.com>
2023-10-30 12:00:45 +0530

    chore: Add log info messages

--- product-service/src/main/java/com/example/controller/ProductController.java
+++ product-service/src/main/java/com/example/controller/ProductController.java
@@ -2,26 +2,31 @@ package com.example.controller;
 
 import com.example.entity.Product;
 import com.example.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class ProductController {
     @Autowired
     ProductService productService;
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getProducts() {
+        log.info("all products service invoked");
         return productService.getProducts();
     }
 
     @GetMapping("/products/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Integer id) {
+        log.info(MessageFormat.format("searching id[{0}] in all products", id));
         return productService.getProductById(id);
     }
 }
--- product-service/src/main/java/com/example/service/ProductService.java
+++ product-service/src/main/java/com/example/service/ProductService.java
@@ -2,15 +2,18 @@ package com.example.service;
 
 import com.example.entity.Product;
 import com.example.repository.ProductRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class ProductService {
 
     @Autowired
@@ -23,6 +26,10 @@ public class ProductService {
 
     public ResponseEntity<Product> getProductById(Integer id) {
         Optional<Product> product = repo.findById(id);
+        if(product.isEmpty()) {
+            log.info(MessageFormat.format("found id[{0}] is not found", id));
+        }
+        log.info(MessageFormat.format("id[{0}] is found", id));
         return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NO_CONTENT).body(new Product()));
     }
 }
